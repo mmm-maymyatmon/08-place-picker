9320fa4c4959d29c7ad262938739a25046030539
mmm-maymyatmon <maymyat.dev@gmail.com>
2025-02-03 22:42:45 +0700

    🔒️ Using useCallback hook
    using useCallback hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
@@ -55,7 +55,7 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -65,7 +65,9 @@ function App() {
       "selectedPlaces",
       JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
     );
-  }
+  }, [])
+
+  
 
   return (
     <>
@@ -73,7 +75,7 @@ function App() {
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
-        />
+        /> 
       </Modal>
 
       <header>
--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(()=> {
+    console.log("time set")
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+    
+
+    return ()=> {
+      console.log("clear timer")
+      clearTimeout(timer);
+    }
+  },[onConfirm])
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
